412a4a4a68443b4210ffa8df54c6543d46b07164
Ajat Darojat <ajatdarojat45@gmail.com>
2023-06-03 04:21:28 +0700

    feat: add cors and body parser

--- app.ts
+++ app.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
+app.use(cors<Request>());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (_: Request, res: Response) => {
   res.status(200).json({ message: `server is running on port` });
 });
 
